63913c7890036f769f63d3273b7eb91c594d49ac
Charles J.Y. Yoon <1242029+jaeyeun97@users.noreply.github.com>
2018-11-15 19:20:42 +0000

    initial

---
+++ main.py
@@ -0,0 +1,28 @@
+
+"""
+0: Exit
+1: Define a Kubernetes Cluster
+    11: Review the cluster Definition
+2: Launch the cluster on AWS
+    21: Validate the cluster
+    22: Deploy the Kubernetes web-dashboard
+    23: Access the Kubernetes web-dashboard
+3: View the cluster
+    31: Get the admin password
+    32: Get the admin service account token
+4: Delete the cluster
+
+Please enter your choice: [#]
+"""
+
+def init():
+    # Do AWS IAM check
+    # Do AWS S3
+    # set kops state & cluster name variable
+    pass
+
+def main():
+    # show the prompt
+    # take input and do the action
+    # iterate
+    pass

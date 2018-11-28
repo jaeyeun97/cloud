bea82d3d238c1fa0201a9202b30c92ef1c36e305
LemonWaterr <swoo@DESKTOP-PPM3G0O.localdomain>
2018-11-28 23:25:07 +0000

    fixing static

--- static/staticScheduler.py
+++ static/staticScheduler.py
@@ -6,22 +6,33 @@ import json
 
 from kubernetes import client, config, watch
 
+log = open('log.txt', 'w+')
+
 #config.load_kube_config()
 config.load_incluster_config()
 v1=client.CoreV1Api()
 
-scheduler_name = "staticScheduler"
+scheduler_name = "staticscheduler"
 
 def getFileSizes():
-    pod_list = v1.list_namespaced_pod("default")
-    custom_master_pods = filter(lambda x : x.metadata.labels["appType"] == "custom_master" and x.status.phase == "Running", pod_list)
-    spark_master_pods = filter(lambda x : x.metadata.labels["appType"] == "spark_master" and x.status.phase == "Running", pod_list)
-    if len(custom_master_pods) == 1 and len(spark_master_pods) == 1:
-        return [int(custom_master_pods[0].metadata.labels["inputSize"]), int(spark_master_pods[0].metadata.labels["inputSize"])]
+    log.write("trying to get filesizes\n")
+    log.flush()
+    custom_master_pods = v1.list_namespaced_pod("default", label_selector="appType==custom_master", limit=1).items
+    spark_master_pods = v1.list_namespaced_pod("default", label_selector="appType==spark_master", limit=1).items
+    log.write("length of cmp: {}\n".format(len(list(custom_master_pods))))
+    log.write("length of smp: {}\n".format(len(list(custom_master_pods))))
+    for v in custom_master_pods:
+        log.write("cmp: appType: {}, phase: {}, inputSize: {}\n".format(v.metadata.labels["appType"], v.status.phase, v.metadata.labels["inputSize"]))
+    log.flush()
+    #TODO: change or to and for running both custom and spark
+    if len(list(custom_master_pods)) == 1 or len(list(spark_master_pods)) == 1:
+        return [int(custom_master_pods[0].metadata.labels["inputSize"]), 0] #int(spark_master_pods[0].metadata.labels["inputSize"])]
     else:
         return [0,0]
 
 def workersAllowed(app, filesizes): #app = 'spark' or 'custom'
+    log.write("getting workersAllowed\n")
+    log.flush()
     #calculating number of nodes to allocate
     #ratio = ourfunct(filesize)
     #spark = round( available * ( ratio / ratio+1 ))
@@ -32,21 +43,27 @@ def workersAllowed(app, filesizes): #app = 'spark' or 'custom'
         return 2
 
 def workersAlreadyRunning(app): #app = 'spark' or 'custom'
-    pod_list = v1.list_namespaced_pod("default")
+    log.write("getting workersAlreadyRunning\n")
+    log.flush()
+    pod_list = v1.list_namespaced_pod("default").items
     #phase can be Pending / Running / Succeeded / Failed / Unknown
     working_pod_list = filter(lambda x : x.metadata.labels["appType"] == app and x.status.phase == "Running", pod_list)
-    return(len(working_pod_list))
+    log.write("wpl length: {}\n".format(len(list(working_pod_list))))
+    log.flush()
+    return(len(list(working_pod_list)))
 
 def nodes_available():
+    log.write("getting nodes available \n")
+    log.flush()
     ready_nodes = []
     for n in v1.list_node().items:
             for status in n.status.conditions:
                 if status.status == "True" and status.type == "Ready":
                     ready_nodes.append(n.metadata.name)
+    log.write("ready_nodes: {}".format(ready_nodes))
     return ready_nodes
 
 def scheduler(name, node, namespace="default"):
-    body=client.V1Binding()
 
     target=client.V1ObjectReference()
     target.kind="Node"
@@ -56,32 +73,48 @@ def scheduler(name, node, namespace="default"):
     meta=client.V1ObjectMeta()
     meta.name=name
 
-    body.target=target
-    body.metadata=meta
+    body=client.V1Binding(metadata=meta, target=target)
+    log.write("meta and target name: {} , {}\n".format(body.metadata.name, body.target.name))
 
-    return v1.create_namespaced_binding_binding(name, namespace, body)
+    return v1.create_namespaced_binding(namespace, body)
 
 def main():
     w = watch.Watch()
     for event in w.stream(v1.list_namespaced_pod, "default"):
+        pod = event['object']
+        log.write("I've got this pod: {}\n".format(pod.metadata.labels['run']))
+        log.write("phase: {} \t scheduler_name: {} \t appType: {} \n".format(pod.status.phase, pod.spec.scheduler_name, pod.metadata.labels['appType']))
+        log.flush()
         appType = event['object'].metadata.labels['appType']
-        if pod.status.phase == "Pending" and pod['object'].spec.scheduler_name == scheduler_name:
+        if pod.status.phase == "Pending" and pod.spec.scheduler_name == scheduler_name:
+            log.write("okay on this pod, let's start \n")
+            log.flush()
             #wait if we don't see both drivers for custom & spark
             fileSizes = []
             while True:
-                fileSizes = getFilesizes()
+                fileSizes = getFileSizes()
                 if fileSizes[0] == 0:
+                    log.write("filesizes: {}".format(fileSizes))
+                    log.flush()
+                    log.write("falling asleep\n")
+                    log.flush()
                     time.sleep(5)
                 else:
                     break
             #check if there's already enough workers or not
-            if workersAlreadyRunning(appType) < workersAllowed(appType):
+            if workersAlreadyRunning(appType) < workersAllowed(appType, [200, 500]):
+                log.write("okay I can assign a node\n")
+                log.flush()
                 try:
                     res = scheduler(event['object'].metadata.name, random.choice(nodes_available()))
                 except client.rest.ApiException as e:
-                    print(json.loads(e.body)['message'])
+                    log.write(json.loads(e.body)['message'])
+                    log.flush()
             else:
+                log.write("I shouldn't assign a node")
+                log.flush()
                 v1.delete_namespaced_pod(event['object'].metadata.name, 'default')
 
 if __name__ == '__main__':
     main()
+    log.close()

5c4588081faa7cc7e4b6e1bc9bf77c20446205a0
LemonWaterr <swoo@DESKTOP-PPM3G0O.localdomain>
2018-11-29 19:27:07 +0000

    spark + staticscheduler works

--- static/scheduler/staticScheduler.py
+++ static/scheduler/staticScheduler.py
@@ -15,23 +15,16 @@ scheduler_name = "staticscheduler"
 podSeen = dict()
 
 def getFileSizes():
-    log.write("trying to get filesizes\n")
-    log.flush()
     custom_master_pods = v1.list_namespaced_pod("default", label_selector="run==group2-custom-master", limit=1).items
     spark_master_pods = v1.list_namespaced_pod("default", label_selector="run==group2-spark-master", limit=1).items
-    log.write("length of cmp: {}\n".format(len(list(custom_master_pods))))
-    log.write("length of smp: {}\n".format(len(list(spark_master_pods))))
-    for v in spark_master_pods:
-        print("cmp: appType: {}, phase: {}, inputSize: {}\n".format(v.metadata.labels["appType"], v.status.phase, v.metadata.labels["inputSize"]))
+    print("got inputsizes/ custom: {} , spark: {}".format(int(custom_master_pods[0].metadata.labels["inputSize"]), 'int(spark_master_pods[0].metadata.labels["inputSize"])'))
     #TODO: change or to and for running both custom and spark
     if len(list(custom_master_pods)) == 1 or len(list(spark_master_pods)) == 1:
-        return [0, int(spark_master_pods[0].metadata.labels["inputSize"])]
+        return [int(spark_master_pods[0].metadata.labels["inputSize"]), 0]
     else:
         return [0,0]
 
 def workersAllowed(app, filesizes): #app = 'spark' or 'custom'
-    log.write("getting workersAllowed\n")
-    log.flush()
     #calculating number of nodes to allocate
     #ratio = ourfunct(filesize)
     #spark = round( available * ( ratio / ratio+1 ))
@@ -50,8 +43,6 @@ def workersAlreadyRunning(app):  # app = 'spark' or 'custom'
 
 
 def nodes_available():
-    log.write("getting nodes available \n")
-    log.flush()
     ready_nodes = []
     for n in v1.list_node().items:
         for status in n.status.conditions:
@@ -83,11 +74,13 @@ def main():
         # labels = {group2-custom-worker, group2-spark-worker}
         if label not in podSeen:
             podSeen[label] = dict()
-        print("Got a pod - name: {}\tphase: {}\tscheduler_name: {}\tlabel: {} \n".format(name, pod.status.phase, pod.spec.scheduler_name, label))
+        print("\nGot a pod - name: {}\tphase: {}\tscheduler_name: {}\tlabel: {}".format(name, pod.status.phase, pod.spec.scheduler_name, label))
 
         #phase = Pending / Running / Succeeded / Failed / Unknown
         if pod.status.phase == 'Succeeded':
             podSeen[label][name] = 'Succeeded'
+        elif pod.status.phase == 'Failed':
+            podSeen[label][name] = 'Failed'
         elif pod.status.phase == "Pending" and pod.spec.scheduler_name == scheduler_name:
             log.write("okay on this pod, let's start \n")
             log.flush()
@@ -95,7 +88,7 @@ def main():
             fileSizes = []
             while True:
                 fileSizes = getFileSizes()
-                if fileSizes[1] == 0:
+                if fileSizes[0] == 0:
                     print("filesizes: {}, falling asleep".format(fileSizes))
                     time.sleep(5)
                 else:
@@ -104,18 +97,24 @@ def main():
             war = workersAlreadyRunning(label)
             print("{} workers already running".format(war))
             if war < workersAllowed(label, [200, 500]):
-                print("okay I can assign a node\n")
-                nodesAvailable = nodes_available()
-                try:
-                    node = random.choice(nodesAvailable)
-                    podSeen[label][name] = 'running'
-                    res = scheduler(name, node)
-                except client.rest.ApiException as e:
-                    log.write(json.loads(e.body)['message'])
-                    log.flush()
+                if name not in podSeen[label]:
+                    print("okay I can assign a node")
+                    nodesAvailable = nodes_available()
+                    try:
+                        node = random.choice(nodesAvailable)
+                        podSeen[label][name] = 'running'
+                        res = scheduler(name, node)
+                    except client.rest.ApiException as e:
+                        log.write(json.loads(e.body)['message'])
+                        log.flush()
+                elif podSeen[label][name] == 'running':
+                    print("I already assigned this pod a node before but this came again, but I shouldn't delete it")
+                else:
+                    print("I've seen this pod before, I marked it either deleted or succeeded but it came again pending. This shouldn't really happen.")
             else:
                 print("I shouldn't assign a node")
                 if name not in podSeen[label]:
+                    print("Deleting this pod")
                     podSeen[label][name] = 'Deleted'
                     v1.delete_namespaced_pod(pod.metadata.name, 'default', delete_option)
 

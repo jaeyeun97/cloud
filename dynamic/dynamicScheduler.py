9229cd6c1398d06ef7de7a57c0a942b46289837e
Charles J.Y. Yoon <1242029+jaeyeun97@users.noreply.github.com>
2018-11-30 02:01:58 +0000

    cmt

--- dynamic/dynamicScheduler.py
+++ dynamic/dynamicScheduler.py
@@ -80,7 +80,7 @@ def main():
                 print("There are no spare nodes or I already assigned a node to this pod")
                 delete(pod.metadata.name)
 
-        elif pod.status.phase == 'Failed' or pod.status.phase = 'Succeeded':
+        elif pod.status.phase == 'Failed' or pod.status.phase == 'Succeeded':
             print("Pod {}, Deleting this pod".format(pod.status.phase))
             delete(pod.metadata.name)
 
